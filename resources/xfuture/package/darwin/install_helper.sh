20833bbadf7374be68d2b64edd950b80da273701
niubir <niubir>
2025-01-10 15:29:05 +0800

    使用dmg

--- resources/xfuture/package/darwin/install_helper.sh
+++ resources/xfuture/package/darwin/install_helper.sh
@@ -6,8 +6,13 @@
 #  Copyright © 2016年 Cenmrev. All rights reserved.
 
 driver_path="$1"
+driver_dir=$(dirname "$driver_path")
+package_dir=$(dirname "$driver_dir")
+xfuture_dir=$(dirname "$package_dir")
+
 sudo mkdir -p "/Library/Application Support/maodou-helper/"
 sudo cp "${driver_path}" "/Library/Application Support/maodou-helper/xsing-box-exec"
 sudo chown root:admin "/Library/Application Support/maodou-helper/xsing-box-exec"
 sudo chmod +s "/Library/Application Support/maodou-helper/xsing-box-exec"
+sudo chmod 777 "${xfuture_dir}/resources"
 echo done
